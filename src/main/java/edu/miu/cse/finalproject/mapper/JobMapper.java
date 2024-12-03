73581c16a16facbe979b452eb86e6df172fee632
Dashzevge <76143002+Dashzevge@users.noreply.github.com>
2024-12-02 20:03:10 -0600

    edit mapper

--- src/main/java/edu/miu/cse/finalproject/mapper/JobMapper.java
+++ src/main/java/edu/miu/cse/finalproject/mapper/JobMapper.java
@@ -4,9 +4,21 @@ import edu.miu.cse.finalproject.dto.job.request.JobRequestDTO;
 import edu.miu.cse.finalproject.dto.job.response.JobResponseDTO;
 import edu.miu.cse.finalproject.model.Job;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface JobMapper {
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "price", target = "price")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "category", target = "category")
     Job toEntity(JobRequestDTO dto);
+
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "price", target = "price")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "category", target = "category")
     JobResponseDTO toResponse(Job entity);
 }

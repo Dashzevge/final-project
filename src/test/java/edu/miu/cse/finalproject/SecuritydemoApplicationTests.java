8e644e6391b35ae6ee64313bcdc7145ec35c9eb4
Dashzevge <76143002+Dashzevge@users.noreply.github.com>
2024-12-05 00:40:49 -0600

    test class

--- src/test/java/edu/miu/cse/finalproject/SecuritydemoApplicationTests.java
+++ src/test/java/edu/miu/cse/finalproject/SecuritydemoApplicationTests.java
@@ -2,7 +2,9 @@ package edu.miu.cse.finalproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class SecuritydemoApplicationTests {
 

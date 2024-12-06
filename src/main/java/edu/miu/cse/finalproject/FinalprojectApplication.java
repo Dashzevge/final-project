11fd9493e6833816a8f0b5655abd2720282114a3
Dashzevge <76143002+Dashzevge@users.noreply.github.com>
2024-12-05 21:15:15 -0600

    main change

--- src/main/java/edu/miu/cse/finalproject/FinalprojectApplication.java
+++ src/main/java/edu/miu/cse/finalproject/FinalprojectApplication.java
@@ -3,7 +3,7 @@ package edu.miu.cse.finalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "edu.miu.cse.finalproject")
 public class FinalprojectApplication {
 
     public static void main(String[] args) {
